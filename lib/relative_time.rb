b7ac3dd74783ac018e4a5d17099e79748666cf70
pinkynrg <biggyapple@gmail.com>
2018-03-07 19:16:15 -0600

    Did some stuff

--- lib/relative_time.rb
+++ lib/relative_time.rb
@@ -1,15 +1,14 @@
-module RelativeTime
+class RelativeTime
 
 	VALID_SIGNS = ['-','+']
 	VALID_ROUND_SIGNS = ["/","|"]
 	VALID_SUBJECTS = { m: 'minute', h: 'hour', d: 'day', w: 'week', M: 'month', y: 'year'}
 
-	def self.parse(str)
-		result = false
+	def initialize(str)
 		# checking string and putting result into groups
 		temp = str.match(/^now(?:([\+-]{1})(\d+)([m|h|d|w|M|y]{1}))?(?:([\/\|]){1}([m|h|d|w|M|y]{1}))?$/)
 		if (temp)
-			result = DateTime.now
+			result = DateTime.now.utc
 			op_sign = temp[1]
 			op_number = temp[2]
 			op_subject = temp[3]
@@ -24,8 +23,32 @@ module RelativeTime
 				result = result.public_send("+", 1.public_send(VALID_SUBJECTS[rnd_subject.to_sym])) if rnd_method == "|"
 				result = result.public_send("beginning_of_#{VALID_SUBJECTS[rnd_subject.to_sym]}")
 			end
+		else
+			begin
+				result = str.to_datetime
+			rescue ArgumentError
+				result = false
+			end		
+		end
+		@result = result
+	end
+
+	def to_graphite
+		@result ? @result.strftime("%H%M_%Y%m%d") : false
+	end
+
+	def to_relative_time
+		result = false
+		if (@result)
+			minutes = ((DateTime.now.utc - @result).to_f.seconds.to_i / 60).to_i
+			sign = minutes > 0 ? "-" : "+"
+			result = "now#{sign}#{minutes.abs}m"
 		end
 		result
+	end
+
+	def to_s
+		@result ? @result.to_s : @result
 	end
 
 end
